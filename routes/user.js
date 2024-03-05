f50418b0edbd603b693b65be2063b57d6486be07
Aditya Patil <adityapatil7649@gmail.com>
2024-03-05 08:03:40 +0530

    Changes in login and signup pages

--- routes/user.js
+++ routes/user.js
@@ -12,11 +12,8 @@ router.post("/signup", wrapAsync(userController.signup));
 
 router.get("/login", userController.renderLoginForm);
 
-
 router.post("/login",saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
 
-
 router.get("/logout", userController.logout)
-
 
 module.exports = router;
